235dcd75155faf56c35d2a512826fe227f4df824
rok93 <goodboy302@naver.com>
2021-06-18 05:02:51 +0900

    done: 공통 인터페이스 기능 - 공통 인터페이스 적용 수강

--- src/test/java/com/example/springdatajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/example/springdatajpa/repository/MemberRepositoryTest.java
@@ -3,6 +3,7 @@ package com.example.springdatajpa.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.springdatajpa.entity.Member;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -31,5 +32,39 @@ class MemberRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(savedMember.getId());
         assertThat(findMember.getUserName()).isEqualTo(savedMember.getUserName());
         assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    void basicCRUD() {
+        //given
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        //then
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        findMember1.setUserName("member!!!!!!!");
+
+        //리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(all).hasSize(2);
+
+        //카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deletedCount = memberRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 }
